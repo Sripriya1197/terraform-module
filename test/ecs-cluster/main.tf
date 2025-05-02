5ec5ff3b305e7cf9bb7367d91185ade8b6eee6d1
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-02 17:35:29 +0530

    Update main.tf

--- test/ecs-cluster/main.tf
+++ test/ecs-cluster/main.tf
@@ -1,17 +1,17 @@
 module "ecs_cluster" {
   source = "../../.modules/aws/ecs"
+  
   cluster_name = "my-ecs-cluster"
-  capacity_providers = ["FARGATE"]
+  capacity_providers = ["FARGATE"] # Check if this is a valid argument for your module
   enable_managed_tags = true
-  propagate_tags = "TASK_DEFINITION"
+  propagate_tags = "TASK_DEFINITION" # Ensure this is valid
 }
 
 module "ecs_task_definition" {
   source = "../../.modules/aws/ecs"
   
-  # ECS Task Definition Configuration
-  family                   = "my-ecs-task"
-  container_definitions    = jsonencode([{
+  family = "my-ecs-task"
+  container_definitions = jsonencode([{
     name      = "my-ecs-container"
     image     = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/github-action:1.1.1"
     essential = true
@@ -20,7 +20,7 @@ module "ecs_task_definition" {
       protocol      = "tcp"
     }]
   }])
-  
+
   cpu                      = "256"
   memory                   = "512"
   requires_compatibilities = ["FARGATE"]
@@ -28,13 +28,15 @@ module "ecs_task_definition" {
 }
 
 module "ecs_service" {
-  source          = "../../.modules/aws/ecs"
-  service_name    = "my-ecs-service" 
-  ecs_cluster_id  = module.ecs_cluster.cluster_id  
-  task_arn        = module.ecs_task_definition.task_definition_arn 
-  count           = 1  
-  fargate         = true  
-  network_config  = {    
+  source = "../../.modules/aws/ecs"
+  
+  name             = "my-ecs-service"
+  cluster          = module.ecs_cluster.cluster_id  # Update `ecs_cluster_id` to `cluster` or the correct name
+  task_definition  = module.ecs_task_definition.task_definition_arn  # Ensure this is the correct reference
+  desired_count    = 1
+  launch_type      = "FARGATE"
+  
+  network_configuration = {
     subnets          = ["subnet-0697385b41cf20408"]
     security_groups  = ["sg-0ef52138839aef07e"]
     assign_public_ip = true
