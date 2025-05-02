aedcc408a0450147f3aa8c569a6d8b9fe936d7e0
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-02 19:35:32 +0530

    Update main.tf

--- test/ecs-cluster/main.tf
+++ test/ecs-cluster/main.tf
@@ -1,64 +1,46 @@
-module "ecs_cluster" {
-  source = "../../.modules/aws/ecs"
-  
-   cluster_name = "my-ecs-cluster"
+module "ecs" {
+  source       = "terraform-aws-modules/ecs/aws"
+  cluster_name = "my-ecs-cluster"
 
-  cluster_configuration = {
-    execute_command_configuration = {
-      logging = "OVERRIDE"
-      log_configuration = {
-        cloud_watch_log_group_name = "my-ecs-cluster-logs" 
+  services = {
+    github-app = {
+      cpu    = 256
+      memory = 512
+
+      container_definitions = {
+        github-container = {
+          image     = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/github-action:1.1.1"
+          essential = true
+          port_mappings = [
+            {
+              containerPort = 80
+              protocol      = "tcp"
+            }
+          ]
+        }
+      }
+
+      subnet_ids = [
+        "subnet-0697385b41cf20408"
+      ]
+
+      security_group_rules = {
+        allow_http = {
+          type        = "ingress"
+          from_port   = 80
+          to_port     = 80
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        allow_all_egress = {
+          type        = "egress"
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
       }
     }
-  }
-
-  fargate_capacity_providers = {
-    FARGATE = {
-      default_capacity_provider_strategy = {
-        weight = 50
-      }
-    }
-    FARGATE_SPOT = {
-      default_capacity_provider_strategy = {
-        weight = 50
-      }
-    }
-  }
-
-  tags = {
-    Environment = "Development"
-    Project     = "MyAppProject"
-  }
-}
-
-module "ecs_container_definition" {
-  source = "../../.modules/aws/ecs"
-  
-  name      = "my-container"
-  cpu       = 256
-  memory    = 512
-  essential = true
-  image = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/github-action:1.1.1"
-    port_mappings = [
-    {
-      name          = "my-container-port"
-      containerPort = 80
-      protocol      = "tcp"
-    }
-  ]
-}
-
-module "ecs_service" {
-  source          = "../../.modules/aws/ecs"
-  service_name    = "my-ecs-service"  
-  cluster_id      = module.ecs_cluster.cluster_id  
-  task_definition = module.ecs_container_definition.task_definition_arn  
-  desired_count   = 1  
-  launch_type     = "FARGATE" 
-
-  network_configuration = {
-    subnets          = ["subnet-0697385b41cf20408"]  
-    security_groups  = ["sg-0ef52138839aef07e"]     
-    assign_public_ip = true  
   }
 }
