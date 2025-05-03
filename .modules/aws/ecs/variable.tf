85dfbbdf171ae3a5766da7e4e0c05c0c9af32194
Sripriya1197 <rsripriyadharshini@gmail.com>
2025-05-03 15:45:20 +0530

    My local updates

--- .modules/aws/ecs/variable.tf
+++ .modules/aws/ecs/variable.tf
@@ -17,6 +17,10 @@ variable "tags" {
 variable "cluster_name" {
   description = "Name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
   type        = string
+<<<<<<< HEAD
+=======
+  default     = "my-ecs-cluster"
+>>>>>>> 9b24b0d (My local updates)
 }
 
 variable "cluster_configuration" {
