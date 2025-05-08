dd95f3be1bfa7b0a3cbc5c042411f31eea47dea2
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-09 00:12:07 +0530

    Update main.tf

--- .modules/aws/ecs/main.tf
+++ .modules/aws/ecs/main.tf
@@ -1,8 +1,4 @@
 module "ecs" {
   source  = "terraform-aws-modules/ecs/aws"
-  version = "5.12.1"
-
-  cluster_name = "my-ecs-tf-cluster" 
-  services     = var.services
-  
+  version = "5.12.1"  
 }
